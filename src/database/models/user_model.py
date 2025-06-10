9f23fd815a71a244bb749e837d0e18e6dfab7529
yarqui <y.pelykh@gmail.com>
2025-06-09 22:31:32 -0400

    add User model

---
+++ src/database/models/user_model.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import List, TYPE_CHECKING
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, DateTime, func
+
+from .base_model import IDOrmModel
+
+if TYPE_CHECKING:
+    from .contact_model import Contact
+
+
+class User(IDOrmModel):
+    __tablename__ = "users"
+
+    # User credential
+    username: Mapped[str] = mapped_column(
+        String(), nullable=False, unique=True, index=True
+    )
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+
+    # User profile info
+    avatar_url: Mapped[str | None] = mapped_column(String(255))
+
+    #  Timestamps
+    created_at: Mapped[datetime] = mapped_column(
+        "created_at",
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        "updated_at",
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+    # Relationships
+    contacts: Mapped[List[Contact]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"<User(id={self.id}, username='{self.username}', email='{self.email}')>"
