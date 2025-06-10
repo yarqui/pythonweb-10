0c1d35f3640efa1955d13159cac676999990c16d
yarqui <y.pelykh@gmail.com>
2025-06-10 14:36:04 -0400

    add user schema, edit contact schema

--- src/schemas/contact_schema.py
+++ src/schemas/contact_schema.py
@@ -7,15 +7,15 @@ class ContactBase(BaseModel):
     last_name: str = Field(max_length=50)
     email: EmailStr
     phone_number: str | None = Field(default=None, max_length=20)
-    birthday: date | None = Field(default=None)
+    birthday: date | None = None
 
 
 class ContactUpdate(BaseModel):
     first_name: str | None = Field(default=None, max_length=50)
     last_name: str | None = Field(default=None, max_length=50)
-    email: EmailStr | None = Field(default=None)
+    email: EmailStr | None = None
     phone_number: str | None = Field(default=None, max_length=20)
-    birthday: date | None = Field(default=None)
+    birthday: date | None = None
 
 
 class ContactResponse(ContactBase):
