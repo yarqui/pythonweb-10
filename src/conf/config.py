2aae5d5fc7e56b426b4108205b53e738d7a867e9
yarqui <y.pelykh@gmail.com>
2025-06-10 07:30:02 -0400

    edit pydantic settings to config

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,7 +1,30 @@
-class Config:
-    SQLALCHEMY_DATABASE_URL = (
-        "postgresql+asyncpg://postgres:finalpassword@localhost:5432/contactapp-db"
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    # These are the default test constants to be used if they are not found in the environment or a .env file.
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "finalpassword"
+    POSTGRES_DB: str = "contactapp-db"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+
+    @property
+    def SQLALCHEMY_DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+
+    # JWT Settings with Defaults
+    JWT_SECRET_KEY: str = "insecure_default_secret_for_testing"
+
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+    # Override the above default settings if .env is found
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
 
-config = Config()
+config = Settings()
