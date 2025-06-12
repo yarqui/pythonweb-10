77ca5a8b04339b1e926afd58ba2e25f8e6e7ab0e
yarqui <y.pelykh@gmail.com>
2025-06-11 21:37:58 -0400

    add user router

---
+++ src/api/user_router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+
+from src.schemas import UserResponse
+from src.database.models import User
+
+from src.services.auth_service import get_current_user
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.get("/me", response_model=UserResponse, summary="Get Current User's Profile")
+async def get_own_profile(current_user: User = Depends(get_current_user)):
+    return current_user
